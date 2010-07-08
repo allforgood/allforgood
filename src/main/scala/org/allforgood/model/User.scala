166c3e699610b0531dc1331213aba2cd1ad3c871
David Pollak <feeder.of.the.bears@gmail.com>
2010-07-08 15:48:53 -0700

    Closes #7.  finished the basic admin functions

--- src/main/scala/org/allforgood/model/User.scala
+++ src/main/scala/org/allforgood/model/User.scala
@@ -12,7 +12,7 @@ import common._
 object User extends User with MetaMegaProtoUser[User] {
   override def dbTableName = "users" // define the DB table name
   override def screenWrap = 
-    Full(<lift:surround with="default" at="content">
+    Full(<lift:surround with="admin" at="content">
 	 <lift:bind /></lift:surround>)
 
   // define the order fields will appear in forms and output
